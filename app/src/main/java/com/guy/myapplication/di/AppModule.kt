ee98880e2c68dbd2423d155caf235076bc97cb54
Guy <guylevi@hotmail.com>
2025-03-28 00:47:32 -0400

    progress

--- app/src/main/java/com/guy/myapplication/di/AppModule.kt
+++ app/src/main/java/com/guy/myapplication/di/AppModule.kt
@@ -3,17 +3,16 @@ package com.guy.myapplication.di
 import com.guy.myapplication.data.manager.SimonSoundManager
 import com.guy.myapplication.ui.viewmodels.SimonGameViewModel
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 /**
  * Main application module for Dependency Injection
  */
 val appModule = module {
-    // Single instance of SimonSoundManager
+    // Single instance of SimonSoundManager using constructor reference
     single { SimonSoundManager(androidContext()) }
 
-    // ViewModel using the factory scope (new instance each time)
-    viewModel { SimonGameViewModel(get()) }
+    // ViewModel using the core module dsl syntax for Koin 4.x
+    viewModelOf(::SimonGameViewModel)
 }
